07dce7b2ad1fc94164dc080bef87d6d36d092f62
エネゴリ もどき <gigamaxstaf@gmail.com>
2025-09-18 12:12:30 +0900

    自分の手と相手の手を入力可能にした。 g,c,p以外の文字を入力した場合は、エラーになるようにした。 勝ち負けの判断を可能にした。 management関数、management2関数の引数にはconstをつけた。

--- GitHubHomeWork3.cpp
+++ GitHubHomeWork3.cpp
@@ -7,12 +7,13 @@ enum Management {
 	G = 0,
 	C = 1,
 	P = 2,
+	None = 3,
 };
 
 enum Management2 {
-	Win = 3,
-	Lose = 4,
-	Draw = 5,
+	Win = 4,
+	Lose = 5,
+	Draw = 6,
 };
 
 Management2 management2(const Management P1, const Management P2) {
@@ -20,9 +21,41 @@ Management2 management2(const Management P1, const Management P2) {
 		((P1 == C && P2 == G) || (P1 == P && P2 == C) || (P1 == G && P2 == P)) ? Lose : Draw;
 }
 
-Management management(char Player) {
-
+Management management(const char Player) {
+	return Player == 'g' ? G :
+		Player == 'c' ? C :
+		Player == 'p' ? P : None;
 }
 
 int main() {
+	char a, b;
+
+	cout << "まずは自分の手を入力してください。>" << flush;
+	cin >> a;
+
+	Management P1 = management(a);
+
+	cout << "次に相手の手を入力してください。>" << flush;
+	cin >> b;
+
+	Management P2 = management(b);
+
+	if (P1 != None && P2 != None) 
+	{
+		Management2 VictoryOrDefeat = management2(P1, P2);
+
+		if (VictoryOrDefeat == Win) {
+			cout << "あなたの勝ちです。" << endl;
+		}
+		else if (VictoryOrDefeat == Lose) {
+			cout << "あなたの負けです。" << endl;
+		}
+		else {
+			cout << "引き分けです。" << endl;
+		}
+	}
+	else {
+		cout << "エラーが起きました。" << endl;
+	}
+}
 
